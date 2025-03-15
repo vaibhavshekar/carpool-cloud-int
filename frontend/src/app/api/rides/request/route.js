53d70b908d5ef81fea9f219751004fbc2898db78
vaibhavshekar <vaibhavvs2004@gmail.com>
2025-03-16 02:00:15 +0530

    amplify

--- frontend/src/app/api/rides/request/route.js
+++ frontend/src/app/api/rides/request/route.js
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/route";
-import prisma from "../../../../../../carpool-app/prisma";
 
 export async function POST(req) {
     try {
