59e6af1d8f7a043666597a40dd6a85365fb9cdb5
vaibhavshekar <vaibhavvs2004@gmail.com>
2025-03-16 01:32:17 +0530

    render main3

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,4 +30,4 @@ def root():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Use Render's assigned port
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
